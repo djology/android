f7e492618ce078a75749e191ce1113f222059af7
djology.w <djology.w@gmail.com>
2021-12-13 01:03:05 +0800

    Initial commit

---
+++ src/main/java/djology/android/AndroidApplication.java
@@ -0,0 +1,18 @@
+package djology.android;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Desc:
+ * @Author: djology.w
+ * @Date: 2021/12/13 0:36
+ */
+@SpringBootApplication
+public class AndroidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AndroidApplication.class, args);
+    }
+
+}
